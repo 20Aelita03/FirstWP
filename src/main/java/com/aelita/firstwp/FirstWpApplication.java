0cccccdfa79789d9f33f41c3dc753c201e9888a6
aires <airesheva@mail.ru>
2023-05-09 23:45:45 +0300

    1 commit

---
+++ src/main/java/com/aelita/firstwp/FirstWpApplication.java
@@ -0,0 +1,13 @@
+package com.aelita.firstwp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstWpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FirstWpApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/aelita/firstwp/FirstWpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aelita.firstwp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstWpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
